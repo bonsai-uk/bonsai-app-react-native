d44ba99576c2099e561c00f45e0a133a68fa7869
Jack Wright <jackw221001@gmail.com>
2023-11-26 21:48:47 +0000

    chore: linting rules

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Text>Bonsai</Text>
+      <Text>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod...
+      </Text>
     </View>
   );
 }
